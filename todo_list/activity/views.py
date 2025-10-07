452249a9cbb315aafc5da3fb742c765121cd4d29
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-07 10:03:18 +0200

    adding jwt token requirement

--- todo_list/activity/views.py
+++ todo_list/activity/views.py
@@ -3,16 +3,19 @@ from django.shortcuts import render
 from rest_framework.decorators import api_view
 from django.core.serializers import serialize
 from . import service
+from rest_framework.permissions import IsAuthenticated
 
 # Create your views here.
 @api_view(['GET'])
 def findAll(request):
+    permission_classes = (IsAuthenticated, )
     activities = service.findAll()
     activities_json = serialize('json', activities)
     return HttpResponse(activities_json, content_type='application/json')
 
 @api_view(['POST'])
 def create(request):
+    permission_classes = (IsAuthenticated, )
     author = request.data['author']
     name = request.data['name']
     date = request.data['date']
@@ -23,6 +26,7 @@ def create(request):
 
 @api_view(['PATCH'])
 def update(request):
+    permission_classes = (IsAuthenticated, )
     name = request.data['name']
     new_name = request.data['new_name']
     new_date = request.data.get('new_date')
@@ -33,6 +37,14 @@ def update(request):
 
 @api_view(['DELETE'])
 def delete(request):
+    permission_classes = (IsAuthenticated, )
     name = request.data['name']
     service.delete(name)
     return HttpResponse('delete method')
+
+# class HelloView(APIView):
+#     permission_classes = (IsAuthenticated, )
+
+#     def get(self, request):
+#         content = {'message': 'Hello, GeeksforGeeks'}
+#         return Response(content)
