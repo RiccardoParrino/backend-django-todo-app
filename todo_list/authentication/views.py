b642e80bcdf3a82877a29e23e33dbfb07e1ef781
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-07 17:14:56 +0200

    create user function

--- todo_list/authentication/views.py
+++ todo_list/authentication/views.py
@@ -1,11 +1,29 @@
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 
+from authentication.models import User
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+
 # Create your views here.
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def login(request):
     return HttpResponse("login response")
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 def create_user(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+    name = request.data.get('name')
+    surname = request.data.get('surname')
+    new_user = User(
+        email=email,
+        password=password,
+        name=name,
+        surname=surname
+    )
+    new_user.save()
     return HttpResponse("create_user response")
