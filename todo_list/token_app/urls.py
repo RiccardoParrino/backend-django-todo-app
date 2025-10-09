7198f280119f72681a7cd66e62454aef339962c0
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-09 09:25:09 +0200

    first customization of TokenObtainPairSerializer

--- todo_list/token_app/urls.py
+++ todo_list/token_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('pair', views.pair, name='pair'),
-    path('refresh', views.refresh, name='refresh'),
-    path('verify', views.verify, name='verify'),
+    # path('pair', views.pair, name='pair'),
+    # path('refresh', views.refresh, name='refresh'),
+    # path('verify', views.verify, name='verify'),
+    path('django', views.MyTokenObtainPairView.as_view(), name='django_token_pair_view')
 ]
