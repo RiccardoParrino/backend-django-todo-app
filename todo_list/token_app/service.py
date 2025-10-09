e4d5ff03b07fe5d0f4a00224da1f1fabc50c7ffd
RiccardoParrino <riccardoparrino1@gmail.com>
2025-10-09 07:10:52 +0200

    adding service layer

---
+++ todo_list/token_app/service.py
@@ -0,0 +1,66 @@
+import datetime
+
+from authentication.models import User
+
+from django.http import HttpResponse, JsonResponse
+
+import jwt
+
+def pair(request):
+    email = request.data.get('email')
+    password = request.data.get('password')
+
+    user = User.objects.get(email=email,password=password)
+
+    if user is not None:
+        payload = {
+            'user_id': user.id,
+            'username': user.email,
+            'name': user.name,
+            'surname': user.surname,
+            'exp': datetime.datetime.now(),
+            'token_type': 'access'
+        }
+
+        refresh_payload = {
+            'user_id': user.id,
+            'username': user.email,
+            'name': user.name,
+            'surname': user.surname,
+            'exp': datetime.datetime.now() + datetime.timedelta(7),
+            'token_type': 'access'
+        }
+
+        token = jwt.encode(payload=payload, key='mySecretKey')
+        refresh_token = jwt.encode(payload=refresh_payload, key='mySecretKey')
+        return JsonResponse({'success':'true', 'access_token':token, 'refresh_token':refresh_token})
+    else:
+        return JsonResponse({'success':'false', 'msg':'The credentials provided are invalid.'})
+
+def refresh(request):
+    token = request.headers['Authorization'].split()[1]
+    try:
+        decoded = jwt.decode(token, key='mySecretKey', algorithms='HS256')
+
+        payload = {
+            'user_id': decoded['user_id'],
+            'username': decoded['username'],
+            'name': decoded['name'],
+            'surname': decoded['surname'],
+            'exp': datetime.datetime.now(),
+            'token_type': 'access'
+        }
+
+        token = jwt.encode(payload=payload, key='mySecretKey')
+
+        return JsonResponse({'success':'true', 'access_token':token})
+    except:
+        return HttpResponse("Invalid token", status='401')
+    
+def verify(request):
+    token = request.headers['Authorization'].split()[1]
+    try:
+        decoded = jwt.decode(token, key='mySecretKey', algorithms='HS256')
+        return HttpResponse("Valid token", status='200')
+    except:
+        return HttpResponse("Invalid token", status='401')
--- todo_list/token_app/views.py
+++ todo_list/token_app/views.py
@@ -9,68 +9,20 @@ from authentication.models import User
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 
+from . import service
+
 # Create your views here.
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def pair(request):
-    email = request.data.get('email')
-    password = request.data.get('password')
-
-    user = User.objects.get(email=email,password=password)
-
-    if user is not None:
-        payload = {
-            'user_id': user.id,
-            'username': user.email,
-            'name': user.name,
-            'surname': user.surname,
-            'exp': datetime.datetime.now(),
-            'token_type': 'access'
-        }
-
-        refresh_payload = {
-            'user_id': user.id,
-            'username': user.email,
-            'name': user.name,
-            'surname': user.surname,
-            'exp': datetime.datetime.now() + datetime.timedelta(7),
-            'token_type': 'access'
-        }
-
-        token = jwt.encode(payload=payload, key='mySecretKey')
-        refresh_token = jwt.encode(payload=refresh_payload, key='mySecretKey')
-        return JsonResponse({'success':'true', 'access_token':token, 'refresh_token':refresh_token})
-    else:
-        return JsonResponse({'success':'false', 'msg':'The credentials provided are invalid.'})
+    return service.pair(request)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def refresh(request):
-    token = request.headers['Authorization'].split()[1]
-    try:
-        decoded = jwt.decode(token, key='mySecretKey', algorithms='HS256')
-
-        payload = {
-            'user_id': decoded['user_id'],
-            'username': decoded['username'],
-            'name': decoded['name'],
-            'surname': decoded['surname'],
-            'exp': datetime.datetime.now(),
-            'token_type': 'access'
-        }
-
-        token = jwt.encode(payload=payload, key='mySecretKey')
-
-        return JsonResponse({'success':'true', 'access_token':token})
-    except:
-        return HttpResponse("Invalid token", status='401')
+    return service.refresh(request)
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
 def verify(request):
-    token = request.headers['Authorization'].split()[1]
-    try:
-        decoded = jwt.decode(token, key='mySecretKey', algorithms='HS256')
-        return HttpResponse("Valid token", status='200')
-    except:
-        return HttpResponse("Invalid token", status='401')
+    return service.verify(request)
